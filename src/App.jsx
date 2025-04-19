ee1e07b99d573b045d47b602a69aea810ae03d98
Riajul Islam <riajulpislam@gmail.com>
2025-04-19 13:49:39 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/collection' element={<Collection/>}/>
@@ -24,6 +27,8 @@ const App = () => {
       <Route path='/place-order' element={<PlaceOrder/>}/>
       <Route path='/orders' element={<Orders/>}/>
     </Routes>
+    </div>
+    
     </>
   )
 }
