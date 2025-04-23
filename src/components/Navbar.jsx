59b7e6da9083425065abeef7a97786fb2d9b3d62
Riajul Islam <riajulpislam@gmail.com>
2025-04-23 21:54:29 +0600

    latest products

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         {/* logo */}
-        <img src={assets.logo} alt="" />
+        <Link to={'/'}> <img src={assets.logo} alt="" /></Link>
         {/* nav */}
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
          <NavLink to={'/'} className='flex flex-col items-center gap-1'>
